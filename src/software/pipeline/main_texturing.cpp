c4611c68ab1f6f63f1962e4bad43fd1a44b35860
jmelou <jean.melou@gmail.com>
2023-09-12 17:30:25 +0200

    [texturing] Allow texturing with normalMaps

--- src/software/pipeline/main_texturing.cpp
+++ src/software/pipeline/main_texturing.cpp
@@ -52,6 +52,8 @@ int aliceVision_main(int argc, char* argv[])
 
     std::string outputFolder;
     std::string imagesFolder;
+    std::string normalsFolder;
+
     image::EImageColorSpace workingColorSpace = image::EImageColorSpace::SRGB;
     image::EImageColorSpace outputColorSpace = image::EImageColorSpace::AUTO;
     bool flipNormals = false;
@@ -75,67 +77,79 @@ int aliceVision_main(int argc, char* argv[])
             "Folder for output mesh");
 
     po::options_description optionalParams("Optional parameters");
-    optionalParams.add_options()
-        ("inputRefMesh", po::value<std::string>(&inputRefMeshFilepath),
-            "Optional input mesh to compute height maps and normal maps. If not provided, no additional maps with geometric information will be generated.")
-        ("imagesFolder", po::value<std::string>(&imagesFolder),
-          "Use images from a specific folder instead of those specify in the SfMData file.\n"
-          "Filename should be the image uid.")
-        ("textureSide", po::value<unsigned int>(&texParams.textureSide)->default_value(texParams.textureSide),
-            "Output texture size")
-        ("downscale", po::value<unsigned int>(&texParams.downscale)->default_value(texParams.downscale),
-            "Texture downscale factor")
-        ("outputMeshFileType", po::value<aliceVision::mesh::EFileType>(&outputMeshFileType)->default_value(aliceVision::mesh::EFileType::OBJ),
-            "output mesh file type")
-        ("colorMappingFileType", po::value<image::EImageFileType>(&texParams.textureFileType)->default_value(texParams.textureFileType),
-          image::EImageFileType_informations().c_str())
-        ("heightFileType", po::value<image::EImageFileType>(&heightFileType)->default_value(image::EImageFileType::NONE),
-            image::EImageFileType_informations().c_str())
-        ("normalFileType", po::value<image::EImageFileType>(&normalFileType)->default_value(image::EImageFileType::NONE),
-            image::EImageFileType_informations().c_str())
-        ("displacementMappingFileType", po::value<image::EImageFileType>(&bumpMappingParams.displacementFileType)->default_value(bumpMappingParams.displacementFileType),
-            image::EImageFileType_informations().c_str())
-        ("bumpType", po::value<mesh::EBumpMappingType>(&bumpMappingParams.bumpType)->default_value(bumpMappingParams.bumpType),
-            "Use HeightMap for displacement or bump mapping")
-        ("unwrapMethod", po::value<std::string>(&unwrapMethod)->default_value(unwrapMethod),
-            "Method to unwrap input mesh if it does not have UV coordinates.\n"
-            " * Basic (> 600k faces) fast and simple. Can generate multiple atlases.\n"
-            " * LSCM (<= 600k faces): optimize space. Generates one atlas.\n"
-            " * ABF (<= 300k faces): optimize space and stretch. Generates one atlas.'")
-        ("useUDIM", po::value<bool>(&texParams.useUDIM)->default_value(texParams.useUDIM),
-            "Use UDIM UV mapping.")
-        ("fillHoles", po::value<bool>(&texParams.fillHoles)->default_value(texParams.fillHoles),
-            "Fill texture holes with plausible values.")
-        ("padding", po::value<unsigned int>(&texParams.padding)->default_value(texParams.padding),
-            "Texture edge padding size in pixel")
-        ("multiBandDownscale", po::value<unsigned int>(&texParams.multiBandDownscale)->default_value(texParams.multiBandDownscale),
-            "Width of frequency bands.")
-        ("multiBandNbContrib", po::value<std::vector<int>>(&texParams.multiBandNbContrib)->default_value(texParams.multiBandNbContrib)->multitoken(),
-             "Number of contributions per frequency band.")
-        ("useScore", po::value<bool>(&texParams.useScore)->default_value(texParams.useScore),
-             "Use triangles scores (based on observations and re-projected areas in source images) for weighting contributions.")
-        ("bestScoreThreshold", po::value<double>(&texParams.bestScoreThreshold)->default_value(texParams.bestScoreThreshold),
-            "(0.0 to disable filtering based on threshold to relative best score).")
-        ("angleHardThreshold", po::value<double>(&texParams.angleHardThreshold)->default_value(texParams.angleHardThreshold),
-            "(0.0 to disable angle hard threshold filtering).")
-        ("workingColorSpace", po::value<image::EImageColorSpace>(&workingColorSpace)->default_value(workingColorSpace),
-            "Color space for the texturing internal computation (does not impact the output file color space).")
-        ("outputColorSpace", po::value<image::EImageColorSpace>(&outputColorSpace)->default_value(outputColorSpace),
-            "Output file colorspace.")
-        ("correctEV", po::value<bool>(&correctEV)->default_value(correctEV),
-            "Option to uniformize images exposure.")
-        ("forceVisibleByAllVertices", po::value<bool>(&texParams.forceVisibleByAllVertices)->default_value(texParams.forceVisibleByAllVertices),
-            "triangle visibility is based on the union of vertices visiblity.")
-        ("flipNormals", po::value<bool>(&flipNormals)->default_value(flipNormals),
-            "Option to flip face normals. It can be needed as it depends on the vertices order in triangles and the convention change from one software to another.")
-        ("visibilityRemappingMethod", po::value<std::string>(&visibilityRemappingMethod)->default_value(visibilityRemappingMethod),
-            "Method to remap visibilities from the reconstruction to the input mesh.\n"
-            " * Pull: For each vertex of the input mesh, pull the visibilities from the closest vertex in the reconstruction.\n"
-            " * Push: For each vertex of the reconstruction, push the visibilities to the closest triangle in the input mesh.\n"
-            " * PullPush: Combine results from Pull and Push results.'")
-        ("subdivisionTargetRatio", po::value<float>(&texParams.subdivisionTargetRatio)->default_value(texParams.subdivisionTargetRatio),
-            "Percentage of the density of the reconstruction as the target for the subdivision (0: disable subdivision, 0.5: half density of the reconstruction, 1: full density of the reconstruction).");
-
+    optionalParams.add_options()("inputRefMesh",
+                                 po::value<std::string>(&inputRefMeshFilepath),
+                                 "Optional input mesh to compute height maps and normal maps. If not provided, no additional maps with geometric "
+                                 "information will be generated.")("imagesFolder",
+                                                                   po::value<std::string>(&imagesFolder),
+                                                                   "Use images from a specific folder instead of those specify in the SfMData file.\n"
+                                                                   "Filename should be the image uid.")(
+      "normalsFolder",
+      po::value<std::string>(&normalsFolder),
+      "Use normal maps from a specific folder to texture the mesh.\n"
+      "Filename should be : uid_normalMap.")(
+      "textureSide", po::value<unsigned int>(&texParams.textureSide)->default_value(texParams.textureSide), "Output texture size")(
+      "downscale", po::value<unsigned int>(&texParams.downscale)->default_value(texParams.downscale), "Texture downscale factor")(
+      "outputMeshFileType",
+      po::value<aliceVision::mesh::EFileType>(&outputMeshFileType)->default_value(aliceVision::mesh::EFileType::OBJ),
+      "output mesh file type")("colorMappingFileType",
+                               po::value<image::EImageFileType>(&texParams.textureFileType)->default_value(texParams.textureFileType),
+                               image::EImageFileType_informations().c_str())(
+      "heightFileType",
+      po::value<image::EImageFileType>(&heightFileType)->default_value(image::EImageFileType::NONE),
+      image::EImageFileType_informations().c_str())("normalFileType",
+                                                    po::value<image::EImageFileType>(&normalFileType)->default_value(image::EImageFileType::NONE),
+                                                    image::EImageFileType_informations().c_str())(
+      "displacementMappingFileType",
+      po::value<image::EImageFileType>(&bumpMappingParams.displacementFileType)->default_value(bumpMappingParams.displacementFileType),
+      image::EImageFileType_informations().c_str())(
+      "bumpType",
+      po::value<mesh::EBumpMappingType>(&bumpMappingParams.bumpType)->default_value(bumpMappingParams.bumpType),
+      "Use HeightMap for displacement or bump mapping")("unwrapMethod",
+                                                        po::value<std::string>(&unwrapMethod)->default_value(unwrapMethod),
+                                                        "Method to unwrap input mesh if it does not have UV coordinates.\n"
+                                                        " * Basic (> 600k faces) fast and simple. Can generate multiple atlases.\n"
+                                                        " * LSCM (<= 600k faces): optimize space. Generates one atlas.\n"
+                                                        " * ABF (<= 300k faces): optimize space and stretch. Generates one atlas.'")(
+      "useUDIM", po::value<bool>(&texParams.useUDIM)->default_value(texParams.useUDIM), "Use UDIM UV mapping.")(
+      "fillHoles", po::value<bool>(&texParams.fillHoles)->default_value(texParams.fillHoles), "Fill texture holes with plausible values.")(
+      "padding", po::value<unsigned int>(&texParams.padding)->default_value(texParams.padding), "Texture edge padding size in pixel")(
+      "multiBandDownscale",
+      po::value<unsigned int>(&texParams.multiBandDownscale)->default_value(texParams.multiBandDownscale),
+      "Width of frequency bands.")(
+      "multiBandNbContrib",
+      po::value<std::vector<int>>(&texParams.multiBandNbContrib)->default_value(texParams.multiBandNbContrib)->multitoken(),
+      "Number of contributions per frequency band.")(
+      "useScore",
+      po::value<bool>(&texParams.useScore)->default_value(texParams.useScore),
+      "Use triangles scores (based on observations and re-projected areas in source images) for weighting contributions.")(
+      "bestScoreThreshold",
+      po::value<double>(&texParams.bestScoreThreshold)->default_value(texParams.bestScoreThreshold),
+      "(0.0 to disable filtering based on threshold to relative best score).")(
+      "angleHardThreshold",
+      po::value<double>(&texParams.angleHardThreshold)->default_value(texParams.angleHardThreshold),
+      "(0.0 to disable angle hard threshold filtering).")(
+      "workingColorSpace",
+      po::value<image::EImageColorSpace>(&workingColorSpace)->default_value(workingColorSpace),
+      "Color space for the texturing internal computation (does not impact the output file color space).")(
+      "outputColorSpace", po::value<image::EImageColorSpace>(&outputColorSpace)->default_value(outputColorSpace), "Output file colorspace.")(
+      "correctEV", po::value<bool>(&correctEV)->default_value(correctEV), "Option to uniformize images exposure.")(
+      "forceVisibleByAllVertices",
+      po::value<bool>(&texParams.forceVisibleByAllVertices)->default_value(texParams.forceVisibleByAllVertices),
+      "triangle visibility is based on the union of vertices visiblity.")(
+      "flipNormals",
+      po::value<bool>(&flipNormals)->default_value(flipNormals),
+      "Option to flip face normals. It can be needed as it depends on the vertices order in triangles and the convention change from one software to "
+      "another.")("visibilityRemappingMethod",
+                  po::value<std::string>(&visibilityRemappingMethod)->default_value(visibilityRemappingMethod),
+                  "Method to remap visibilities from the reconstruction to the input mesh.\n"
+                  " * Pull: For each vertex of the input mesh, pull the visibilities from the closest vertex in the reconstruction.\n"
+                  " * Push: For each vertex of the reconstruction, push the visibilities to the closest triangle in the input mesh.\n"
+                  " * PullPush: Combine results from Pull and Push results.'")(
+      "subdivisionTargetRatio",
+      po::value<float>(&texParams.subdivisionTargetRatio)->default_value(texParams.subdivisionTargetRatio),
+      "Percentage of the density of the reconstruction as the target for the subdivision (0: disable subdivision, 0.5: half density of the "
+      "reconstruction, 1: full density of the reconstruction).");
 
     CmdLine cmdline("AliceVision texturing");
     cmdline.add(requiredParams);
@@ -241,6 +255,14 @@ int aliceVision_main(int argc, char* argv[])
         denseMesh.load(inputRefMeshFilepath);
 
         mesh.generateNormalAndHeightMaps(mp, denseMesh, outputFolder, bumpMappingParams);
+    }
+
+    // generate normal maps textures from the fusion of normal maps per image
+    if (!normalsFolder.empty() && bumpMappingParams.bumpMappingFileType != image::EImageFileType::NONE)
+    {
+        ALICEVISION_LOG_INFO("Generate normal maps.");
+        mvsUtils::MultiViewParams mpN(sfmData, normalsFolder);
+        mesh.generateTextures(mpN, outputFolder, hwc.getMaxMemory(), texParams.textureFileType, mvsUtils::EFileType::normalMap);
     }
 
     // save final obj file
